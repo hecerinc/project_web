21a6191f50c9afd5f36afdc1e1ac9bd68b6e791f
Hector Rincon <hecerinc@hotmail.com>
2018-11-12 00:47:53 -0600

    Add database

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # Proyecto Web
 
 Twitter clone para la clase de web.
+
+## Database
+
+- Users
+	- username
+	- password
+	- email
+	- name
+	- created
+	- modified
+	- profilePicturePath
+	- coverPhotoPath
+- Tweets
+	- body
+	- retweet_id
+	- created
+	- modified
+- Followers
+	- from
+	- to
+- Likes
+	- user_id
+	- tweet_id
+
---
+++ data/db.sql
@@ -0,0 +1,44 @@
+-- @author Hector Rincon
+
+CREATE DATABASE IF NOT EXISTS proyectoweb CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+CREATE TABLE users (
+	id int(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	username VARCHAR(255) UNIQUE NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	name VARCHAR(255),
+	created DATETIME,
+	profilePicturePath TEXT,
+	coverPhotoPath TEXT,
+	modified DATETIME
+) Engine=InnoDB;
+
+
+CREATE TABLE tweets (
+	id int(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	body TEXT,
+	retweet_id int(11) UNSIGNED NOT NULL,
+	created DATETIME,
+	modified DATETIME
+) Engine=InnoDB;
+
+
+CREATE TABLE followers (
+	from_id int(11) UNSIGNED NOT NULL,
+	to_id int(11) UNSIGNED NOT NULL,
+	PRIMARY KEY(from_id, to_id),
+	FOREIGN KEY(from_id) REFERENCES users(id),
+	FOREIGN KEY(to_id) REFERENCES users(id)
+) Engine=InnoDB;
+
+CREATE TABLE likes (
+	user_id int(11) UNSIGNED NOT NULL,
+	tweet_id int(11) UNSIGNED NOT NULL,
+	PRIMARY KEY(user_id, tweet_id),
+	FOREIGN KEY(user_id) REFERENCES users(id),
+	FOREIGN KEY(tweet_id) REFERENCES tweets(id)
+) Engine=InnoDB;
+
+ALTER TABLE tweets ADD CONSTRAINT fk_retweet_id FOREIGN KEY(retweet_id) REFERENCES tweets(id);
+
